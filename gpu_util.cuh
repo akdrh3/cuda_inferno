50fa49c704f9c7e62f279ef6d74db035bcf31336
akdrh3 <im7738@naver.com>
2024-10-30 13:54:39 +0100

    make file

--- gpu_util.cuh
+++ gpu_util.cuh
@@ -5,7 +5,7 @@
 
 #define HANDLE_ERROR(err) (HandleError(err, __FILE__, __LINE__))
 
-__device__ void print_array_host(int *array, int64_t array_size);
+__device__ void print_array_device(int *array, int64_t array_size);
 __host__ void HandleError(cudaError_t err, const char *file, int line);
 __host__ void cuda_timer_start(cudaEvent_t *start, cudaEvent_t *stop);
 __host__ float cuda_timer_stop(cudaEvent_t start, cudaEvent_t stop);
--- mergesort.cu
+++ mergesort.cu
@@ -11,9 +11,9 @@ void mergesortkernel(){
 
 }
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
     //read file
-    const char file_name = argv[1];
+    const char *file_name = argv[1];
     uint64_t size_of_array = strtoull(argv[2], NULL, 10);
 
     printf("file name %s, size of the array : %llu", file_name, size_of_array);
@@ -22,7 +22,9 @@ void main(int argc, char *argv[]){
     //different thread number
 
     //launch mergesort kernel
-    //measure time
+    //measure time 
+
+    return 0;
 
 }
 
