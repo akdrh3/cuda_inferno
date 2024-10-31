85fdb8e7375a02b6b63610e867a70f8a110489cc
akdrh3 <im7738@naver.com>
2024-10-31 03:30:53 +0100

    initial mergesort

--- mergesort.cu
+++ mergesort.cu
@@ -2,6 +2,7 @@
 extern "C"{
 #include "util.h"
 }
+#include <cmath>
 
 __device__ void swap(int &a, int &b)
 {
@@ -15,20 +16,79 @@ void swap_int_pointer(int **arr_A, int **arr_B, bool flipped){
     int *tmp_pointer=*arr_A;
     *arr_A = *arr_B;
     *arr_B = tmp_pointer;
-    flipped = !flipped;
+    *flipped = !(*flipped);
     //printf("swapped pointer \n\n");
 }
 
+void mergesortkernel(int* arr, int* tmp, uint64_t size_of_array, uint64_t segment_size){
+    //getting tid, start, mid, and end index
+    uint64_t tid = threadIdx.x + blockDim.x * blockIdx.x;  
 
-void mergesortkernel(){
+    // based on tide, devide and get the portion of the array that this specific tid has to work on
+    // everything is in index number
+    uint64_t start = tid * segment_size; //last tid * segment_size = size_of_array - segment_size
+
+    // Ignore out-of-bounds threads
+    if (start > size_of_array -1){
+        //printf("out of bound thread happening even though it should not in very frist step unless the array size is smaller than 256.");
+        return;  
+    }
+
+    //mid means array_B starting index
+    uint64_t mid = min(start + segment_size, size_of_array -1);
+    //end means array_B ending index
+    uint64_t end = min(start + segment_size*2 - 1, size_of_array -1);
+
+    if(start < end){
+        uint64_t array_a_index = start, array_b_index = mid, temp_index = start;
+        //printf("inside merge; index1 : %lu, index2 : %lu, tmp index: %lu, end: %lu\n", start, mid, start, end);
+        while (array_a_index < mid && array_b_index <= end){
+            if (arr[array_a_index] <= arr[array_b_index]){
+                tmp[temp_index++] = arr[array_a_index++];
+            } 
+            else{
+                tmp[temp_index++] = arr[array_b_index++];
+            }
+        }
+
+        while (array_a_index < mid){
+            tmp[temp_index++] = arr[array_a_index++];
+        }
+
+        while (array_b_index <= end){
+            tmp[temp_index++] = arr[array_b_index++];
+        } 
+    }
+
 
 }
 
-void mergesort(){
+void mergesort(int *arr, int *tmp, uint64_t size_of_array, int number_of_thread){
+    //calculate the array size that initially goes into each thread
+    uint64_t segment_size = (uint64_t)ceil((double)size_of_array / number_of_thread);
+    bool flipped = false;
+    
 
+    //sort the smallest portion of the sorting
+    initial_merge<<<1, number_of_thread>>>(arr, tmp, size_of_array, segment_size);
+    HANDLE_ERROR(cudaDeviceSynchronize());
+
+    //now it is sure that the smallest segments are sorted
+    //inter-segments mergesort
+    segment_size *= 2;
+    while (segment_size <= size_of_array){
+        mergesortKernel<<<1, number_of_thread>>>(arr, tmp, size_of_array, segment_size);
+        HANDLE_ERROR(cudaDeviceSynchronize());
+        swap_int_pointer(&arr, &tmp, &flipped);
+        segment_size *= 2;
+    }
+
+    if (flipped == true){
+        swap_int_pointer(&arr, &tmp, &flipped);
+    }
 }
 
-__global__ void initial_merge(int* arr, int* tmp, uint64_t size_of_array, uint64_t segment_size, bool flipped)
+__global__ void initial_merge(int* arr, int* tmp, uint64_t size_of_array, uint64_t segment_size)
 {
     //getting tid, start, mid, and end index
     uint64_t tid = threadIdx.x + blockDim.x * blockIdx.x;
@@ -75,8 +135,6 @@ __global__ void initial_merge(int* arr, int* tmp, uint64_t size_of_array, uint64
                 for (uint64_t i = left_start; i <= right_end; i++) {
                     arr[i] = tmp[i];
                 }
-                // swap_int_pointer(int **arr, int **tmp, bool flipped);
-                // printf("flipped: %d", flipped);
             }
         }
     }
@@ -93,7 +151,6 @@ int main(int argc, char *argv[]){
     int *gpu_tmp = NULL;
     HANDLE_ERROR(cudaMallocManaged((void**)&gpu_array, size_of_array * sizeof(int)));
     HANDLE_ERROR(cudaMallocManaged((void **)&gpu_tmp, size_of_array * sizeof(int)));
-    bool flipped = false;
     cudaEvent_t start, stop;
 
     //different thread number
@@ -109,16 +166,12 @@ int main(int argc, char *argv[]){
         //read from file and store it to gpu_array
         read_from_file(file_name, gpu_array, size_of_array);
         print_array_host(gpu_array, size_of_array);
-
-        //calculate the array size that initially goes into each thread
-        uint64_t segment_size = (uint64_t)ceil((double)size_of_array / number_of_thread);
-        
+ 
         //start timer
         cuda_timer_start(&start, &stop);
 
-        //sort the smallest portion of the sorting
-        initial_merge<<<1, number_of_thread>>>(gpu_array, gpu_tmp, size_of_array, segment_size, flipped);
-        HANDLE_ERROR(cudaDeviceSynchronize());
+        //call mergesort function
+        mergesort(gpu_array, gpu_tmp, size_of_array, number_of_thread);
 
         // Stop timer
         double gpu_sort_time = cuda_timer_stop(start, stop);
