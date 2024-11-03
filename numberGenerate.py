c8450173b37a280c096fe370962e6eae94d9f5a3
akdrh3 <im7738@naver.com>
2024-11-03 22:18:39 +0100

    debug

--- numberGenerate.py
+++ numberGenerate.py
@@ -4,31 +4,24 @@ from datetime import datetime
 
 input_a = int(input("Enter the size of numbers: "))
 
-start_time = time.time()
 # Get the current time
 current_time = datetime.now()
-
-# Format the time
 formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-# Print the formatted time
 print("Current Time:", formatted_time)
 
-f = open("numbers_{input_a}.txt", "w")
+start_time = time.time()
+
+file_name = f"numbers_{input_a}.txt"
 data_size = input_a * 1000000
-for i in range(0,data_size):
-    num = r.randint(0,2147483647)
-    f.write("%d\n"%num)
+with open(file_name, "w") as f:
+    for i in range(0,data_size):
+        num = r.randint(0,2147483647)
+        f.write("%d\n"%num)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Elapsed time for generating data set: {elapsed_time} sec")
 
-# Get the current time
 current_time = datetime.now()
-
-# Format the time
 formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-# Print the formatted time
 print("Current Time:", formatted_time)
