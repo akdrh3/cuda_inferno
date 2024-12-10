fe619e670b876e711dab79e62e98c121dd6bff1c
akdrh3 <im7738@naver.com>
2024-12-10 21:30:49 +0100

    thrust

--- thrust_merge.cu
+++ thrust_merge.cu
@@ -13,16 +13,32 @@ int main(int argc, char *argv[])
 {
     // get param from command; filename , arraysize * 1 million
     const char *file_name = argv[1];
-    uint64_t input_size = strtoull(argv[2], NULL, 10) * 10;
-    int *a = (int *)malloc(sizeof(int) * input_size);
-    if (a == NULL)
+    uint64_t input_size = strtoull(argv[2], NULL, 10) * 100;
+    int *host_a = (int *)malloc(sizeof(int) * input_size);
+    if (host_a == NULL)
     {
         return -1;
     }
-    read_from_file_cpu(file_name, a, input_size);
-    print_array_host(a, input_size);
+    read_from_file_cpu(file_name, host_a, input_size);
 
-    free(a);
+    size_t pinned_size = 1000000; // Limited by pinned memory size
+    size_t numChunks = input_size / pinned_size;
+
+    int *h_aPinned;
+    HANDLE_ERROR(cudaMallocHost((void **)&h_aPinned, pinned_size));
+
+    int *d_a;
+    HANDLE_ERROR(cudaMalloc((void **)&d_a, input_size));
+
+    cudaStream_t stream1, stream2;
+    cudaStreamCreate(&stream1);
+    cudaStreamCreate(&stream2);
+
+    free(host_a);
+    cudaFreeHost(h_aPinned);
+    cudaStreamDestroy(stream1);
+    cudaStreamDestroy(stream2);
+    cudaFree(d_a);
 
     return 0;
 }
