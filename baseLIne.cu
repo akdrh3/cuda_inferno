52a652b437a6f8027c6ba8c3e424fef02a261f9a
akdrh3 <im7738@naver.com>
2024-12-12 12:43:21 +0100

    thrust

--- baseLIne.cu
+++ baseLIne.cu
@@ -35,34 +35,52 @@ int main(int argc, char *argv[])
     int *d_a;
     HANDLE_ERROR(cudaMalloc((void **)&d_a, sizeof(int) * input_size));
 
-    uint64_t batch_size = 5 * 1000000000;
+    uint64_t batch_size = 10000000;
     uint64_t numChunks = (input_size + batch_size - 1) / batch_size;
     thrust::device_ptr<int> dev_ptr;
 
-    cudaEvent_t event, start, stop, gpu_start, gpu_stop, dtoh_start, dtoh_stop;
+    cudaEvent_t event, start, stop, gpu_start, gpu_stop, dtoh_start, dtoh_stop, cpu_start, cpu_stop;
     cudaEventCreate(&event);
 
     cuda_timer_start(&start, &stop);
     cuda_timer_start(&gpu_start, &gpu_stop);
     for (uint64_t i = 0; i < numChunks; i++)
     {
-        uint64_t left_size = (i < numChunks - 1) ? batch_size : (input_size % batch_size);
         uint64_t offset = i * batch_size;
+        uint64_t left_size = std::min(batch_size, input_size - offset);
 
         HANDLE_ERROR(cudaMemcpy(d_a + offset, host_a + offset, left_size * sizeof(int), cudaMemcpyHostToDevice));
         dev_ptr = thrust::device_pointer_cast(d_a + offset);
         thrust::sort(dev_ptr, dev_ptr + left_size);
+        HANDLE_ERROR(cudaDeviceSynchronize());
     }
     double gpu_time = cuda_timer_stop(gpu_start, gpu_stop) / 1000.0;
     cuda_timer_start(&dtoh_start, &dtoh_stop);
     HANDLE_ERROR(cudaMemcpy(host_b, d_a, input_size * sizeof(int), cudaMemcpyDeviceToHost));
     double dtoh_time = cuda_timer_stop(dtoh_start, dtoh_stop) / 1000.0;
-    // HANDLE_ERROR(cudaMemcpy(host_b, d_a, input_size * sizeof(int), cudaMemcpyDeviceToHost));
-    print_array_host(host_b, 10);
-    printf("sorted : %d \n", isRangeSorted_cpu(host_b, 0, batch_size - 1));
+
+    cuda_timer_start(&cpu_start, &cpu_stop);
+    std::vector<int> merged_result(input_size);
+    uint64_t current_offset = 0;
+
+    for (uint64_t i = 0; i < numChunks - 1; i++)
+    {
+        uint64_t left_size = std::min(batch_size, input_size - current_offset);
+        uint64_t next_offset = current_offset + left_size;
+        uint64_t next_size = std::min(batch_size, input_size - next_offset);
+
+        std::merge(host_b + current_offset,
+                   host_b + current_offset + left_size,
+                   host_b + next_offset,
+                   host_b + next_offset + next_size,
+                   merged_result.begin() + current_offset);
+        current_offset += left_size;
+    }
+    double cpu_time = cuda_timer_stop(cpu_start, cpu_stop) / 1000.0;
 
     double total_time = cuda_timer_stop(start, stop) / 1000.0;
-    printf("Total time: %lf, gpu sort: %lf, dtoh : %lf\n", total_time, gpu_time, dtoh_time);
+    printf("Total time: %lf, gpu sort: %lf, dtoh : %lf cpu sort: %lf\n", total_time, gpu_time, dtoh_time, cpu_time);
+    printf("sorted : %d \n", isRangeSorted_cpu(host_b, 0, input_size - 1));
 
     free(host_a);
     free(host_b);
