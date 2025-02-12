a7a79970383994907ee15b5dcf70fb0e3cd3e0e6
akdrh3 <im7738@naver.com>
2025-02-11 22:29:22 -0500

    data isSorted added

--- cpu.cpp
+++ cpu.cpp
@@ -5,25 +5,38 @@
 #include <cstdlib> // For std::atoi
 
 std::vector<double> readDoublesFromFile(const std::string &filename, size_t numElements);
-
+bool isSorted(const std::vector<double> &data)
+{
+    for (size_t i = 0; i < data.size() - 1; ++i)
+    {
+        if (data[i] > data[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 4)
     {
-        std::cerr << "Usage: " << argv[0] << " <filename> <number of elements>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <filename> <number of elements> <thread>" << std::endl;
         return 1; // Exit if not enough arguments
     }
 
-    std::string filename = argv[1];          // First argument is the filename
-    size_t numElements = std::atoi(argv[2]); // Second argument is the number of elements to read
+    std::string filename = argv[1];                    // First argument is the filename
+    size_t numElements = std::atoi(argv[2]) * 1000000; // Second argument is the number of elements to read
 
     std::vector<double> data = readDoublesFromFile(filename, numElements);
+    std::cout << "data is " << (isSorted(data) ? "sorted." : "not sorted.") << std::endl;
 
-    // Output the contents of the vector
-    for (double num : data)
-    {
-        std::cout << num << std::endl;
-    }
+    // Set the number of threads
+    omp_set_num_threads(argv[3]); // Adjust the number of threads like 16 or 20 as per your scenario
+
+    // Now call the parallel sort
+    __gnu_parallel::sort(data.begin(), data.end());
+
+    std::cout << "data is " << (isSorted(data) ? "sorted." : "not sorted.") << std::endl;
 
     return 0;
 }
