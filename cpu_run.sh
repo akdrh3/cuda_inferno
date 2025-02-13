c82bbad6391af308d886795963e222cf97e5115b
akdrh3 <im7738@naver.com>
2025-02-12 23:59:08 -0500

    workload

--- cpu_run.sh
+++ cpu_run.sh
@@ -5,11 +5,12 @@ elements_list=(100 200 400 800 1600 3200 6400 12800 25600 51200 102400 204800 40
 
 # Filename and number of threads
 filename="12800numbers.txt"
-threads=16
+workloads=(0 0.2 0.4 0.6 0.8 1)
 
 # Loop through each element in the elements list
 for num_elements in "${elements_list[@]}"
 do
-    echo "Running ./cpu with $num_elements elements"
-    ./cpu $filename $num_elements $threads
+    for workload in "${workloads[@]}"
+        echo "Running ./cpu with $num_elements elements with $workload cpu workload"
+        ./cpu $filename $num_elements $workload
 done
