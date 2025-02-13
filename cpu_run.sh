a78160c4798da48dfb6f18a7b90eb16b564c11c7
akdrh3 <im7738@naver.com>
2025-02-13 00:02:12 -0500

    workload

--- cpu_run.sh
+++ cpu_run.sh
@@ -13,6 +13,8 @@ workloads=(0 0.2 0.4 0.6 0.8 1)
 for num_elements in "${elements_list[@]}"
 do
     for workload in "${workloads[@]}"
-        echo "Running ./cpu with $num_elements elements with $workload cpu workload"
+    do
+        echo "Running ./workload with $num_elements elements with $workload cpu workload"
         ./workload $filename $num_elements $workload
+    done
 done
